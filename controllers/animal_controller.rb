f2bda952696864331cc73980e0e1ce21bac5f809
MashR3 <michaelash17@googlemail.com>
2017-09-28 23:21:46 +0100

    Index, New, Show

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -9,28 +9,46 @@ class AnimalController < Sinatra::Base
 	set :views, Proc.new { File.join(root, 'views') }
 
 $creatures = [{
-		title: "Phineas the Phoenix"
+		title: "Phineas the Phoenix",
+		descr: "Fire resistant, likes to sleep in the fireplace",
+		dob: "400BC"
 	},
 	{
-		title: "Uriah the Unicorn"
+		title: "Uriah the Unicorn",
+		descr: "Shnes bright like a diamond, pokes holes in the curtains",
+		dob: "250BC"
 	},
 	{
-		title: "Dinah the Dragon"
+		title: "Dinah the Dragon",
+		descr: "Too large to keep indoors, flies away to feast but will return",
+		dob: "March 1892"
 	},
 	{
-		title: "Gareth the Griffin"
+		title: "Gareth the Griffin",
+		descr: "Slight identity issues, enjoys discussing politics and race relations",
+		dob: "December 1990"
 	},
 	{
-		title: "Feta the Fairy"
-
+		title: "Feta the Fairy",
+		descr: "Very clean, can use pixie dust to assist with chores, sweet-tempered",
+		dob: "May 1999"
 	}]
 
 	get '/animals' do 
 		@page_header = "Current Adoptable Creatures"
-		@animal = $creatures
-		erb :"animals/index.erb"
+		@animals = $creatures
+		erb :"animals/index"
 	end
 
+	get '/animals/new' do
+		erb :'animals/new'
+	end
+
+	get '/animals/:id' do 
+		id = params[:id].to_i
+		@animal = $creatures[id]
+		erb :'animals/show'
+	end
 
 
 
